8a5b2481bcabbd43a1771c7809e3d3a99fbb6185
kaoko <kaoko28@gmail.com>
2019-09-19 15:27:50 -0400

    all models working

---
+++ client/build/precache-manifest.ccb295709658db8b826cba45882cad73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d58c019facd01feeccabe1f41893a9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b7a0cad32d749b67d78",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "60d54c66eb6cf52bb511",
+    "url": "/static/js/2.ae6dacf0.chunk.js"
+  },
+  {
+    "revision": "4b7a0cad32d749b67d78",
+    "url": "/static/js/main.b7702672.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
