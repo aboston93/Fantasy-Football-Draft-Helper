d098a4e82b870b164b2974bad8cb56d60f91a030
kaoko <kaoko28@gmail.com>
2019-09-19 10:45:16 -0400

    second  model testing

---
+++ controllers/players.js
@@ -0,0 +1,92 @@
+/* Step 1 import express
+ *
+ */
+const express = require('express')
+
+
+/* Step 2
+ *
+ * Import the api files from the models
+ *
+ * TODO: change the file path to the models file you'll need to use.
+ * TODO: rename this from `templateApi` to something more sensible (e.g:
+ * `shopsAPI`)
+ *
+ * NOTE: You may need to import more than one API to create the 
+ * controller you need.
+ * 
+ */
+const playerApi = require('../models/players.js')
+
+
+/* Step 3 
+ * 
+ * Create a new router.
+ *
+ * the router will "contain" all the request handlers that you define in this file.
+ * TODO: rename this from templateRouter to something that makes sense. (e.g:
+ * `shopRouter`)
+ */
+const playerRouter = express.Router()
+
+
+/* Step 4
+ * 
+ * TODO: Put all request handlers here
+ */
+
+
+
+
+
+// playerRouter.route("/fantasylist")
+//     .get((req, res) => {
+//         res.send(playerApi.getAllPlayers());
+// templateRouter.get('/', (req, res) => {
+//     res.send(templateApi.getHelloWorldString())
+//   })
+
+//     })
+playerRouter.get("/fantasylist", (req, res) => {
+    playerApi.getAllPlayers().then(x =>
+        res.send(x))
+})
+
+playerRouter.post("/fantasylist", (req, res) => {
+    playerApi.addNewPlayer(req.body).then(x =>
+        res.send(x))
+})
+
+
+playerRouter.delete("/:id", (req, res) => {
+    playerApi.DeletePlayer(req.params.id,).then(x =>
+        res.send(x))
+
+})
+
+
+playerRouter.put("/:id", (req, res) => {
+    playerApi.updatePlayer(req.params.id,req.body).then(x =>
+        res.send(x))
+
+})
+
+
+
+
+
+
+
+
+
+
+// shopsRouter.route('/')
+//   .get((req, res) => {
+//     res.send(shopsApi.getShops());
+
+//   })
+
+module.exports = {
+    playerRouter,
+
+}
---
+++ models/detail.js
@@ -0,0 +1,120 @@
+
+
+
+/* 
+* Place all functions, classes, and/or DB schemas here for a single 
+* model.
+*/
+
+/* Step 1
+ *
+ * TODO: import mongoose connection
+ * NOTE: skip this if you are not using mongoose
+ *
+ */
+const mongoose = require('./connection.js')
+
+/* Step 1 alternative
+ *
+ * TODO: make a global variable to act as an in memory database. 
+ * NOTE: doing this WILL NOT persist your data and you will loose
+ * your data once you stop running your server.
+ *
+ */
+
+
+/* Step 2
+ *
+ * TODO: create model schema 
+ * NOTE: skip this if you are not using mongoose
+ *
+ */
+let DetailSchema = mongoose.Schema({
+    Pros: String,
+    Cons: String,
+    stats: String,
+ 
+    
+});
+
+
+
+
+/* Step 3
+ *
+ * TODO: create collection API
+ * NOTE: skip this if you are not using mongoose
+ *
+ */const DetailCollection = mongoose.model('detail', DetailSchema)
+
+
+/* Step 4
+ *
+ * TODO: delete this it's just a sample
+ *
+ */
+// function getHelloWorldString() {
+//   return 'hello world'
+// }
+const CreateDetail = () =>
+DetailCollection.create({
+        Pros: "Accurate Pocket passer on on great team",
+        Cons: "diffrent player when facing blitzes",
+        stats: "500tds in a career",
+     
+        
+    })
+
+function getAllDetails() {
+    return DetailCollection.find()
+}
+function addNewDetail(newdetail) {
+    //make sure that the isActive is either true or false.
+    //if you're using a checkbox in an HTML form then
+    //if the checkbox is unselected and you submit the form
+    //isActive will not be in the account object
+
+    return DetailCollection.create(newdetail);
+}
+
+function DeleteDetail(detailid) {
+
+    return DetailCollection.deleteOne({ _id: detailid });
+
+}
+
+
+function GetDetail(detailid) {
+    return DetailCollection.findById(detailid);
+}
+
+function updateDetail(detailid, updateddetail) {
+    //make sure that the isActive is either true or false.
+    //if you're using a checkbox in an HTML form then
+    //if the chec kbox is unselected and you submit the form
+    //isActive will not be in the account object
+
+
+    return DetailCollection.findByIdAndUpdate(detailid, updateddetail)
+}
+//       const CreateAuthor = () =>
+//   AuthorCollection.create({ name: "One Punch Man", created: new Date(), genre: "Harem ", StartDate: new Date(),
+//   EndDate:new Date(), mainCharacter:"Saitama" , type:"shounen", creator:"mikashi" 
+//       })
+
+// function getAllAuthor() {
+//     return AuthorCollection.find()
+//   }
+
+//  * TODO: export all functions from this file by adding their names as keys to this
+//  * object
+//  */
+module.exports = {
+    CreateDetail,
+    getAllDetails,
+    addNewDetail,
+    DeleteDetail,
+    GetDetail,
+    updateDetail
+
+}
