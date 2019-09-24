6c3652c8ad41e232b254103abd1144c001ff640e
kaoko <kaoko28@gmail.com>
2019-09-24 08:27:10 -0400

    hal

---
+++ client/build/precache-manifest.083c49f51b6d7fc766127f234cf39abb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3f776942bebf64f789f26f2c94c0bf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85da0fcc69586f8d4a38",
+    "url": "/static/css/main.e2aaec7c.chunk.css"
+  },
+  {
+    "revision": "8ec09d00a687dd28d61d",
+    "url": "/static/js/2.cce7aa39.chunk.js"
+  },
+  {
+    "revision": "85da0fcc69586f8d4a38",
+    "url": "/static/js/main.901f11d8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
