5f6ccf398f2a1c8d4f37def9123b9f0a8d34f548
kaoko <kaoko28@gmail.com>
2019-09-24 10:03:38 -0400

    tring to get heroku to work

---
+++ client/build/precache-manifest.27425c0eddd22dee75d662eea6b9f1e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e10a5bfc12ac64ce48d1267f6868f424",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ca484aa45bc8b1cd406",
+    "url": "/static/css/main.7866c4e4.chunk.css"
+  },
+  {
+    "revision": "8ec09d00a687dd28d61d",
+    "url": "/static/js/2.cce7aa39.chunk.js"
+  },
+  {
+    "revision": "6ca484aa45bc8b1cd406",
+    "url": "/static/js/main.889db0b5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
